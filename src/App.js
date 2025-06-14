d0c553a72ff95b4d0e7c36c293e65f64e2959569
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2025-06-14 14:08:30 -0500

    Implement book creation functionality in App component and integrate BookList for displaying books

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 export default function componentName() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log("New Book Created", title);
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math, random() * 9999), title },
+    ];
+    setBooks(updatedBooks);
   };
 
   return (
-    <>
+    <div className="app">
+      <BookList books={books} />
       <BookCreate onCreate={createBook} />
-    </>
+    </div>
   );
 }
