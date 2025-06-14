471d0cd47862ca0948016ae5f05b9fb5acf2c2de
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2025-06-14 14:20:42 -0500

    Add deleteBookById function to App component and pass onDelete prop to BookList

--- src/App.js
+++ src/App.js
@@ -8,14 +8,21 @@ export default function componentName() {
   const createBook = (title) => {
     const updatedBooks = [
       ...books,
-      { id: Math.round(Math, random() * 9999), title },
+      { id: Math.round(Math.random() * 9999), title },
     ];
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
     setBooks(updatedBooks);
   };
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
